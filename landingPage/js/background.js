10d680366c8bfa0c333af1bf386b3d6c43a25935
marco ramirez <77829612+mramirez0610@users.noreply.github.com>
2022-01-17 17:07:07 -0500

    color change
    also time for test on mobile

--- landingPage/js/background.js
+++ landingPage/js/background.js
@@ -1,5 +1,7 @@
 let mainArray = [];
-let count = 15;
+let count = 20;
+var xoff1 = 0;
+var xoff2 = 1000000;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -12,18 +14,19 @@ function setup() {
 }
 
 function draw() {
-    background(100);
+    background(160);
     circleSetup(); 
 }
 
 function circleSetup() {
-    const junk = [ "#BFC3BA", "#A9ACA9", "#60495A", "#3F3244", "#2F2235" ];
-    const colorList = [...junk, ...junk, ...junk];
+    //const junk = [ "#BFC3BA", "#A9ACA9", "#60495A", "#3F3244", "#2F2235" ];
+    const junk = [ "#F3E9E9", "#E4D0CF", "#A3BBCE", "#3F5178", "#D3E5EE" ];
+    const colorList = [...junk, ...junk, ...junk, ...junk];
 
     //creates a circle for every entry in the array
     for (var q = 0; q < colorList.length; q++) {
         var curColor = color(colorList[q]);
-        curColor.setAlpha(220);
+        curColor.setAlpha(200);
         fill(curColor);
 
         mainArray[q].create();
@@ -34,26 +37,33 @@ function circleSetup() {
 
 class coolCircle {
     constructor() {
-        this.d = random(40, 200);
-        this.x = random(0, windowWidth);
-        this.y = random(0, windowHeight);
+        this.d = random(40, 250);
+        //literally just found out about the noise function this is fucking cool
+        this.x = map(noise(xoff1), 0, 1, 10, width)
+        this.y = map(noise(xoff2), 0, 1, 10, height) 
+        xoff1 += 1;
+        xoff2 += 50;
         this.speed = 0;
     }
-
+    
     //this looks fucking gross, but it works! doesnt let it go too fast
     float() {
         if (this.speed <= 15) {
             this.y += this.speed;
+            this.x += this.speed / 3;
             this.speed -= this.d / 20000;
         }
         if (this.speed <= -15) {
-            this.y += this.speed
+            this.y += this.speed;
             this.speed += 20;
         }
     }
     rules() {
         if (this.y < -200) {
             this.y = height + 100;
+        }
+        if (this.x < - 200){
+            this.x = width + 100;
         }
     }
     create() {
